b4b82ee7e3b6a8ccbdf8e039525bb599e08e168f
OctCarp <octcarp@hotmail.com>
2023-05-29 02:27:24 +0800

    refactor: move codes to root directory

--- dhcp.py
+++ dhcp.py
@@ -25,70 +25,76 @@ class Config():
 
 
 class DHCPServer():
+    #  class variables
     hardware_addr = Config.controller_macAddr
     server_ip = Config.server_ip
     dns = Config.dns
     start_ip = Config.start_ip
     end_ip = Config.end_ip
-    start_ip_i = ip_to_int(start_ip)
-    end_ip_i = ip_to_int(end_ip)
+    start_ip_i = ip_to_int(start_ip)  # 32 bits number for start IP
+    end_ip_i = ip_to_int(end_ip)  # 32 bits number for start IP
     netmask = Config.netmask
-    lease_time = Config.lease_time
+    lease_time = Config.lease_time  # default lease time
 
-    ip_mac = {}
-    for i in range(start_ip_i, end_ip_i + 1):
-        ip_mac[i] = 'ok'
+    ip_mac = {}  # Map between IP and host MAC
+    for i in range(start_ip_i, end_ip_i + 1):  # initialization
+        ip_mac[i] = 'ok'  # If it is 'OK', meanings the IP is available
 
+    # do some initialization for byte type data below
     server_ip_byte = addrconv.ipv4.text_to_bin(server_ip)
     netmask_byte = addrconv.ipv4.text_to_bin(netmask)
     dns_byte = addrconv.ipv4.text_to_bin(dns)
     lease_time_byte = struct.pack('>I', lease_time)
 
+    # nack_byte = struct.pack('>B', )
     offer_byte = struct.pack('>B', dhcp.DHCP_OFFER)
     ack_byte = struct.pack('>B', dhcp.DHCP_ACK)
-
-    # nack_byte = struct.pack('>B', )
 
     @classmethod
     def check_ip_mac(cls, req_ip_i, client_mac):
         ip_return = '0.0.0.0'
         if (not req_ip_i == 0) and (cls.ip_mac[req_ip_i] == client_mac or cls.ip_mac[req_ip_i] == 'ok'):
+            # if it has required IP and it is available
+            for ip_i in cls.ip_mac:
+                if cls.ip_mac[ip_i] == client_mac:
+                    cls.ip_mac[ip_i] = 'ok'  # clear the previous IP info for this client
             cls.ip_mac[req_ip_i] = client_mac
-            ip_return = int_to_ip(req_ip_i)
+            ip_return = int_to_ip(req_ip_i)  # return the IP string
         else:
             has_mac = False
             for ip_i in cls.ip_mac:
                 if cls.ip_mac[ip_i] == client_mac:
-                    ip_return = int_to_ip(ip_i)
+                    ip_return = int_to_ip(ip_i)  # has previous IP information for the client
                     has_mac = True
                     break
             if not has_mac:
                 for ip_i in cls.ip_mac:
-                    if cls.ip_mac[ip_i] == 'ok':
+                    if cls.ip_mac[ip_i] == 'ok':  # has available IP for new MAC
                         cls.ip_mac[ip_i] = client_mac
                         ip_return = int_to_ip(ip_i)
                         break
-        return ip_return
+        return ip_return  # return IP string in the end
 
     @classmethod
     def assemble_offer(cls, pkt):
+        # get each layer for the packet
         c_eth = pkt.get_protocol(ethernet.ethernet)
         c_ipv4 = pkt.get_protocol(ipv4.ipv4)
         c_udp = pkt.get_protocol(udp.udp)
         c_dhcp = pkt.get_protocol(dhcp.dhcp)
 
-        client_mac = c_eth.src
+        client_mac = c_eth.src  # get client MAC for IP-MAC mapping
 
         offer_pkt = packet.Packet()
         offer_pkt.add_protocol(ethernet.ethernet(
-            ethertype=c_eth.ethertype,
+            ethertype=c_eth.ethertype,  # sync
             dst=client_mac,  # client mac
             src=cls.hardware_addr  # controller mac
         ))
 
         offer_pkt.add_protocol(ipv4.ipv4(
-            version=c_ipv4.version,
-            proto=c_ipv4.proto,
+            version=c_ipv4.version,  # sync
+            proto=c_ipv4.proto,  # sync
             src=cls.server_ip,  # dhcp server ip
             dst='255.255.255.255'  # broadcast addr
         ))
@@ -101,10 +107,10 @@ class DHCPServer():
         req_ip_i = 0
 
         for opt in c_dhcp.options.option_list:
-            if opt.tag == dhcp.DHCP_REQUESTED_IP_ADDR_OPT:  # required ip address
-                req_ip_i = int.from_bytes(opt.value, byteorder='big')
+            if opt.tag == dhcp.DHCP_REQUESTED_IP_ADDR_OPT:  # if it has required IP address
+                req_ip_i = int.from_bytes(opt.value, byteorder='big')  # unpack IP information
 
-        offer_return_ip = cls.check_ip_mac(req_ip_i, client_mac)
+        offer_return_ip = cls.check_ip_mac(req_ip_i, client_mac)  # get IP for client
 
         offer_pkt.add_protocol(dhcp.dhcp(
             op=dhcp.DHCP_BOOT_REPLY,  # 2
@@ -115,27 +121,27 @@ class DHCPServer():
             ciaddr='0.0.0.0',
             yiaddr=offer_return_ip,  # Your (client) IP address
             siaddr=cls.server_ip,  # Server IP address
-            chaddr=c_dhcp.chaddr,  # Client hardware address
+            chaddr=c_dhcp.chaddr,  # Client hardware address (MAC addr)
             options=dhcp.options([
-                dhcp.option(tag=dhcp.DHCP_MESSAGE_TYPE_OPT,
-                            value=cls.offer_byte
+                dhcp.option(tag=dhcp.DHCP_MESSAGE_TYPE_OPT,  # set message type as offer
+                            value=cls.offer_byte  # byte for number 2
                             ),
                 dhcp.option(tag=dhcp.DHCP_IP_ADDR_LEASE_TIME_OPT,
-                            value=cls.lease_time_byte
+                            value=cls.lease_time_byte  # add lease time info
                             ),
                 dhcp.option(tag=dhcp.DHCP_SERVER_IDENTIFIER_OPT,
-                            value=cls.server_ip_byte
+                            value=cls.server_ip_byte  # add server identifier
                             ),
                 dhcp.option(tag=dhcp.DHCP_SUBNET_MASK_OPT,
-                            value=cls.netmask_byte
+                            value=cls.netmask_byte  # add subnet info
                             ),
                 dhcp.option(tag=dhcp.DHCP_DNS_SERVER_ADDR_OPT,
-                            value=cls.dns_byte
+                            value=cls.dns_byte  # add DNS info
                             )
             ])
         ))
 
-        return offer_pkt
+        return offer_pkt  # return the packet finally
 
     @classmethod
     def assemble_ack(cls, pkt):
@@ -173,7 +179,7 @@ class DHCPServer():
         ack_return_ip = cls.check_ip_mac(req_ip_i, client_mac)
 
         ack_pkt.add_protocol(dhcp.dhcp(
-            op=dhcp.DHCP_BOOT_REPLY,  # 5
+            op=dhcp.DHCP_BOOT_REPLY,
             htype=1,  # ethernet
             hlen=c_dhcp.hlen,
             xid=c_dhcp.xid,  # random transaction id, define by client
