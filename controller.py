5e926f78ffd369d9360228cb2873319c8130cce6
OctCarp <octcarp@hotmail.com>
2023-05-31 23:02:29 +0800

    feat(controller): shortest path

--- controller.py
+++ controller.py
@@ -1,68 +1,127 @@
-from ryu.base import app_manager
+from collections import defaultdict
 from ryu.controller import ofp_event
 from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
-from ryu.topology import event, switches
 from ryu.ofproto import ofproto_v1_0
-from ryu.lib.packet import packet, ethernet, ether_types, arp
+from ryu.lib.packet import arp
 from ryu.lib.packet import dhcp
 from ryu.lib.packet import ethernet
-from ryu.lib.packet import ipv4
 from ryu.lib.packet import packet
-from ryu.lib.packet import udp
+from ryu.topology.api import *
 
 from dhcp import DHCPServer
 
-class ControllerApp(app_manager.RyuApp):
+from queue import Queue
+
+
+class ControllerAPP(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_0.OFP_VERSION]
 
     def __init__(self, *args, **kwargs):
-        super(ControllerApp, self).__init__(*args, **kwargs)
+        super(ControllerAPP, self).__init__(*args, **kwargs)
+        self.arp_table = {}
+        self.swids = []
+        self.host_port = {}
+        self.adj = defaultdict(lambda: defaultdict(lambda: None))
+        self.hosts = {}
+        self.port_state = {}
 
-    @set_ev_cls(event.EventSwitchEnter)
-    def handle_switch_add(self, ev):
-        """
-        Event handler indicating a switch has come online.
-        """
+    def clear(self):
+        self.swids = []
+        self.adj = defaultdict(lambda: defaultdict(lambda: None))
 
-    @set_ev_cls(event.EventSwitchLeave)
-    def handle_switch_delete(self, ev):
-        """
-        Event handler indicating a switch has been removed
-        """
+    def update_topo(self):
+        self.clear()
+        self.swids = [sw.dp.id for sw in get_all_switch(self)]
 
+        links_list = get_all_link(self)
+        for link in links_list:
+            self.adj[link.src.dpid][link.dst.dpid] = link.src.port_no
+            self.adj[link.dst.dpid][link.src.dpid] = link.dst.port_no
 
-    @set_ev_cls(event.EventHostAdd)
-    def handle_host_add(self, ev):
-        """
-        Event handler indiciating a host has joined the network
-        This handler is automatically triggered when a host sends an ARP response.
-        """ 
-        # TODO:  Update network topology and flow rules
+        for cur_switch in self.swids:
+            for host_mac in self.host_port.keys():
+                host_swid = self.host_port[host_mac][0]
+                host_port_no = self.host_port[host_mac][1]
+                sw_port = self.shortest(cur_switch, host_swid, host_port_no)
+                if sw_port:
+                    for sw_id, out_port in sw_port:
+                        dp = get_switch(self, sw_id)[0].dp
+                        match = dp.ofproto_parser.OFPMatch(dl_dst=host_mac)
+                        actions = [dp.ofproto_parser.OFPActionOutput(out_port)]
+                        mod = dp.ofproto_parser.OFPFlowMod(datapath=dp, match=match,
+                                                           priority=1, actions=actions)
+                        dp.send_msg(mod)
 
-    @set_ev_cls(event.EventLinkAdd)
-    def handle_link_add(self, ev):
-        """
-        Event handler indicating a link between two switches has been added
-        """
-        # TODO:  Update network topology and flow rules
+                for src_mac in self.host_port.keys():
+                    if self.host_port[src_mac][0] == cur_switch:
+                        src_port = self.host_port[src_mac]
+                        if sw_port and self.port_state[(src_port[0], src_port[1])]:
+                            self.print_path(sw_port=sw_port, src_mac=src_mac, dst_mac=host_mac)
+                        elif src_mac != host_mac:
+                            print(f"Net is break for {src_mac} to {host_mac}")
 
-    @set_ev_cls(event.EventLinkDelete)
-    def handle_link_delete(self, ev):
-        """
-        Event handler indicating when a link between two switches has been deleted
-        """
-        # TODO:  Update network topology and flow rules
-   
-        
+    def print_path(self, src_mac, sw_port, dst_mac):
+        if sw_port[0][0] != self.host_port[dst_mac][0]:
+            cnt = -1
+            info = f"src_mac: {src_mac} -> "
+            for sw_id, out_port in sw_port:
+                info = info + f"s{sw_id} -> "
+                cnt = cnt + 1
+            info = info + f"dst_mac: {dst_mac}"
+            info = info + f", switch dis = {cnt}"
+            print(info)
 
-    @set_ev_cls(event.EventPortModify)
-    def handle_port_modify(self, ev):
-        """
-        Event handler for when any switch port changes state.
-        This includes links for hosts as well as links between switches.
-        """
-        # TODO:  Update network topology and flow rules
+    def shortest(self, src_sw, dst_sw, dst_port):
+        if not self.port_state[(dst_sw, dst_port)]:
+            return None
+
+        if src_sw == dst_sw:
+            return [(dst_sw, dst_port)]
+
+        dis = {}
+        fa = {}
+
+        nodes = self.swid
+        for node in nodes:
+            dis[node] = float('inf')
+            fa[node] = None
+
+        que = Queue()
+        que.put(src_sw)
+        dis[src_sw] = 0
+        while not que.empty():
+            cur = que.get()
+            for sw in nodes:
+                if self.adj[cur][sw] is not None and dis[sw] > dis[cur] + 1:
+                    dis[sw] = dis[cur] + 1
+                    fa[sw] = cur
+                    que.put(sw)
+
+        path_ids = []
+        if dst_sw not in fa.keys():
+            return None
+
+        father = fa[dst_sw]
+        cur = dst_sw
+        while True:
+            if cur == src_sw:
+                path_ids.append(src_sw)
+                break
+            elif father is None:
+                return None
+            else:
+                path_ids.append(cur)
+                father = fa[cur]
+                cur = father
+        path_ids.reverse()
+
+        sw_port = []
+        for step in range(0, len(path_ids) - 1):
+            out_port = self.adj[path_ids[step]][path_ids[step + 1]]
+            sw_port.append((path_ids[step], out_port))
+        sw_port.append((dst_sw, dst_port))
+        return sw_port
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def packet_in_handler(self, ev):
@@ -110,4 +169,39 @@ class ControllerApp(app_manager.RyuApp):
                 DHCPServer.handle_dhcp(datapath, inPort, pkt)
         except Exception as e:
             self.logger.error(e)
-    
+
+    @set_ev_cls(event.EventSwitchEnter)
+    def handle_switch_add(self, ev):
+        print("Add Switch")
+        self.update_topo()
+
+    @set_ev_cls(event.EventSwitchLeave)
+    def handle_switch_delete(self, ev):
+        print("Delete Switch")
+        self.update_topo()
+
+    @set_ev_cls(event.EventHostAdd)
+    def handle_host_add(self, ev):
+        print("Add Host")
+        host_mac = ev.host.mac
+        self.host_port[host_mac] = (ev.host.port.dpid, ev.host.port.port_no)
+        self.port_state[(ev.host.port.dpid, ev.host.port.port_no)] = True
+        self.update_topo()
+
+    @set_ev_cls(event.EventLinkAdd)
+    def handle_link_add(self, ev):
+        print("Add Link")
+        self.update_topo()
+
+    @set_ev_cls(event.EventLinkDelete)
+    def handle_link_delete(self, ev):
+        print("Delete Link")
+        self.update_topo()
+
+    @set_ev_cls(event.EventPortModify)
+    def handle_port_modify(self, ev):
+        print("Modify Port")
+        for port in self.port_state:
+            if (ev.port.dpid, ev.port.port_no) == port:
+                self.port_state[(ev.port.dpid, ev.port.port_no)] = ev.port.is_live()
+        self.update_topo()
